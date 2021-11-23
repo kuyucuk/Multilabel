25a5e2ceda38bd09265ffd48ede6099da022b771
totokuyu <tolga_k94@hotmail.com>
2021-11-23 12:16:09 +0600

    update

--- tez_kod/Makine Öğrenmesi/git.py
+++ tez_kod/Makine Öğrenmesi/git.py
@@ -1,8 +1,12 @@
 import os
 import sys
 import io
+import datetime
 
 os.system('git add .')
 os.system('git commit -a -m update')
 os.system('git push')
+
+tarihsaat = datetime.datetime.now().strftime("%d-%m-%Y %H:%M");
+print(tarihsaat)
 print("git pushing completed")
