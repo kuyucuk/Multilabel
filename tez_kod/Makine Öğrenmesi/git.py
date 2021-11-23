7029b49403226b40d0174fdf6e095a56403216fb
totokuyu <tolga_k94@hotmail.com>
2021-11-23 12:21:42 +0600

    update 23-11-2021 12:21

--- tez_kod/Makine Öğrenmesi/git.py
+++ tez_kod/Makine Öğrenmesi/git.py
@@ -7,7 +7,7 @@ tarihsaat = datetime.datetime.now().strftime("%d-%m-%Y %H:%M");
 tarih = datetime.datetime.now().strftime("%d-%m-%Y");
 
 os.system('git add .')
-os.system('git commit -a -m "update '+str(tarih)+'"')
+os.system('git commit -a -m "update '+str(tarihsaat)+'"')
 os.system('git push')
 
 print(tarih)
